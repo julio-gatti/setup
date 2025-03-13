f1dfef1e33c7eb412ba85c88babf6f1bd543bf7b
Julius Sakki <53840016+julio-gatti@users.noreply.github.com>
2025-03-13 09:30:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,15 +11,19 @@ sudo apt install build-essential clang-format cmake
 ## Development Libraries
 sudo apt install libsdl2-dev libglfw3-dev libopenal-dev
 
-##
+## Editing
 sudo snap install blender --classic
 sudo apt install gimp
 sudo snap install trenchbroom
+
+## Audio Editing
+sudo snap install audacity
 
 ## Games
 sudo snap install steam
 sudo apt install quake2 quake3
 
+## ZIP Editing
 ## https://slade.mancubus.net/index.php?page=wiki&wikipage=Installation
 sudo wget -P /etc/apt/sources.list.d https://debian.drdteam.org/drdteam-$(dpkg --print-architecture).sources
 sudo apt-get install slade
