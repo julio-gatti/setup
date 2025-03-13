ada29c4f6733f2e5e86a359c782a38a8fe926c98
Julius Sakki <53840016+julio-gatti@users.noreply.github.com>
2025-03-14 01:59:20 +0200

    Update raspberrypi.sh

--- raspberrypi.sh
+++ raspberrypi.sh
@@ -6,3 +6,9 @@ sudo apt update
 sudo apt install pcmanfm
 
 sudo apt install libreoffice
+
+## Building
+sudo apt install build-essential clang-format cmake git
+
+## Development Libraries
+sudo apt install libsdl2-dev libglfw3-dev libopenal-dev libfltk1.3-dev libassimp-dev
