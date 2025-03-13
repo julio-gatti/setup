32ac53f4480ba00d2b2ffefa85be672afc383dbe
Julius Sakki <53840016+julio-gatti@users.noreply.github.com>
2025-03-13 05:23:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
+# Setup
+
 apt update
 
+##
 snap install vlc
 
+##
 apt install build-essential clang-format cmake
 
+## Development Libraries
 apt install libsdl2-dev
 
+##
 snap install blender --classic
 
+## Games
 apt install quake2 quake3
