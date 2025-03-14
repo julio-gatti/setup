6edd4c06d55b95398196ed7fc111dbbe88fb6dbe
Julius Sakki <53840016+julio-gatti@users.noreply.github.com>
2025-03-14 02:06:38 +0200

    Update raspberrypi.sh

--- raspberrypi.sh
+++ raspberrypi.sh
@@ -7,6 +7,10 @@ sudo apt install pcmanfm
 
 sudo apt install libreoffice
 
+## Editing
+sudo apt install gimp
+#sudo apt install blender # requires OpenGL 3.3 :(
+
 ## Building
 sudo apt install build-essential clang-format cmake git
 
