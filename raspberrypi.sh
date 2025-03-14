84daf53951d79273c66828e75d263a167af4afa0
Julius Sakki <53840016+julio-gatti@users.noreply.github.com>
2025-03-14 02:01:59 +0200

    Update raspberrypi.sh

--- raspberrypi.sh
+++ raspberrypi.sh
@@ -1,4 +1,4 @@
-# Raspberry Pi Setup
+# Raspberry Pi (Debian/Raspbian) Setup
 
 sudo apt update
 
@@ -12,3 +12,6 @@ sudo apt install build-essential clang-format cmake git
 
 ## Development Libraries
 sudo apt install libsdl2-dev libglfw3-dev libopenal-dev libfltk1.3-dev libassimp-dev
+
+## Games
+sudo apt install assaultcube quake2 quake3 darkplaces
